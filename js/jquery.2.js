e26513eaa878e2b636e30f72d5d260182038b01c
okirigabriel24@gmail.com <okirigabriel24@gmail.com>
2020-08-26 10:31:22 +0100

    made changes

--- js/jquery.2.js
+++ js/jquery.2.js
@@ -18,3 +18,21 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
+
+
+$(document).ready(function () {
+        
+	$('.first-button').on('click', function () {
+	
+	  $('.animated-icon1').toggleClass('open');
+	});
+	$('.second-button').on('click', function () {
+	
+	  $('.animated-icon2').toggleClass('open');
+	});
+	$('.third-button').on('click', function () {
+	
+	  $('.animated-icon3').toggleClass('open');
+	});
+	
+	});
--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ $('.counter').counterUp({
   });
 
 
+
+  
+
+
       // review-active
 $('.testmonial_active ').owlCarousel({
 	loop:true,
@@ -107,3 +111,26 @@ $('.testmonial_active ').owlCarousel({
 		}
 	}
   });
+
+
+
+
+
+
+  
+  $(document).ready(function () {
+        
+	$('.first-button').on('click', function () {
+	
+	  $('.animated-icon1').toggleClass('open');
+	});
+	$('.second-button').on('click', function () {
+	
+	  $('.animated-icon2').toggleClass('open');
+	});
+	$('.third-button').on('click', function () {
+	
+	  $('.animated-icon3').toggleClass('open');
+	});
+	
+	});
